b3810c4840d67c6fc25443073a4c209b04ff4a9b
Moharamxr <sim.MohamedMostafa13137@alexu.edu.eg>
2024-12-11 15:37:57 +0200

    first commit

---
+++ app/_data/static-data.js
@@ -0,0 +1,124 @@
+export const Airlines = [
+  {
+    value: "AA",
+    label: "American Airlines",
+  },
+  {
+    value: "AS",
+    label: "Alaska Airlines",
+  },
+  {
+    value: "B6",
+    label: "JetBlue Airways",
+  },
+  {
+    value: "DL",
+    label: "Delta Air Lines",
+  },
+  {
+    value: "F9",
+    label: "Frontier Airlines",
+  },
+  {
+    value: "G4",
+    label: "Allegiant Air",
+  },
+  {
+    value: "HA",
+    label: "Hawaiian Airlines",
+  },
+  {
+    value: "NK",
+    label: "Spirit Airlines",
+  },
+  {
+    value: "SY",
+    label: "Sun Country Airlines",
+  },
+  {
+    value: "UA",
+    label: "United Airlines",
+  },
+  {
+    value: "WN",
+    label: "Southwest Airlines",
+  },
+];
+
+export const itemTypes = [
+  {
+    category: "Personal Items",
+    items: [
+      "Wallet",
+      "Mobile Phone",
+      "Passport",
+      "ID Card",
+      "Credit Cards",
+      "Glasses/Sunglasses",
+      "Keys",
+      "Jewelry",
+      "Wristwatches",
+    ],
+  },
+  {
+    category: "Electronics",
+    items: [
+      "Laptop",
+      "Headphones",
+      "Tablet",
+      "Camera",
+      "Charger/Cable",
+      "Power Bank",
+    ],
+  },
+  {
+    category: "Luggage & Bags",
+    items: ["Handbag", "Backpack", "Suitcase", "Gym Bag", "Briefcase"],
+  },
+  {
+    category: "Clothing & Personal Items",
+    items: ["Coat/Jacket", "Hat", "Scarf", "Gloves", "Shoes"],
+  },
+  {
+    category: "Documents",
+    items: ["Travel Tickets", "Documents", "Certificates"],
+  },
+  {
+    category: "Children's Items",
+    items: ["Stroller", "Toy", "Diaper Bag", "Baby Bottle"],
+  },
+  {
+    category: "Sports & Leisure Items",
+    items: ["Sports Equipment", "Books/Magazines", "Musical Instruments"],
+  },
+  {
+    category: "Miscellaneous Items",
+    items: [
+      "Water Bottle",
+      "Umbrella",
+      "Cosmetics/Makeup",
+      "Medical Supplies",
+      "Travel Accessories",
+    ],
+  },
+];
+
+export const locationOptions = [
+  {
+    category: "Common Locations",
+    items: [
+      "Security Checkpoint",
+      "Departure Lounge",
+      "Arrival Hall",
+      "Gate",
+      "Baggage Claim Area",
+      "Check-in Counters",
+      "Elevators/Escalators",
+      "Parking Area",
+      "Restaurants and Cafes",
+      "Restrooms",
+      "Duty-Free Shops",
+      "Customer Service Desk",
+    ],
+  },
+];
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,25 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
+        background: "#F9F9F9",
         foreground: "var(--foreground)",
+        primary: "var(--color-primary)",
+        primaryLight: "var(--color-primary-light)",
+        primaryHover: "var(--color-primary-hover)",
+        discard: "var(--color-discard)",
+        discardHover: "var(--color-discard-hover)",
+        secondary: "var(--color-secondary)",
+        secondaryHover: "var(--color-secondary-hover)",
+        headlines: "var(--color-headlines)",
+        subtitle: "var(--color-subtitle)",
+        info: "var(--color-info)",
+        navItem: "var(--color-nav-item)",
+        navActive: "var(--color-primary)",
+        error: "var(--color-error)",
+        border: "var(--color-border)",
+      },
+      fontFamily: {
+        helveticaNeue: ["var(--font-helvetica-neue)"],
       },
     },
   },
